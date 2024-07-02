d918ef34a82a9a3384d238b503e44d67360e5f97
Lian Ivan Sinchez <liam.sinchez00@gmail.com>
2024-07-02 15:29:18 -0300

    canciones

--- js/likes.js
+++ js/likes.js
@@ -93,19 +93,24 @@ let albums = [
     new Song(new Audio("../songs/haggstrom.mp3"), "Haggstrom", "C418", "../imgs/haggstrom.jpg"), 
     new Song(new Audio("../songs/wethands.mp3"), "Wet Hands", "C418", "../imgs/wethands.jpg")], "Minecraft", "C418", "../imgs/covers/minecraft.jpg", "Game Soundtrack"),
 
-    new Album([new Song(new Audio("../songs/dryhands.mp3"), "Dry Hands", "Luciana", "../imgs/dryhands.jpg"), 
-    new Song(new Audio("../songs/haggstrom.mp3"), "Haggstrom", "Luciana", "../imgs/haggstrom.jpg"), 
-    new Song(new Audio("../songs/wethands.mp3"), "Wet Hands", "Luciana", "../imgs/wethands.jpg")], "Minecraft", "Luciana", "../imgs/covers/minecraft.jpg", "Rock"),
-
-    new Album([new Song(new Audio("../songs/dryhands.mp3"), "Dry Hands", "Lian", "../imgs/dryhands.jpg"), 
-    new Song(new Audio("../songs/haggstrom.mp3"), "Haggstrom", "Lian", "../imgs/haggstrom.jpg"), 
-    new Song(new Audio("../songs/wethands.mp3"), "Wet Hands", "Lian", "../imgs/wethands.jpg")], "Minecraft", "Lian", "../imgs/covers/minecraft.jpg", "Progressive Rock"),
-
     new Album([
         new Song(new Audio("../songs/wewillrockyou.mp3"), "We Will Rock You", "Queen", "../imgs/covers/queen-newsoftheworld.jpeg"),
         new Song(new Audio("../songs/wearechampions.mp3"), "We Are The Champions", "Queen", "../imgs/covers/queen-newsoftheworld.jpeg"), new Song(new Audio("../songs/sheerheartattack.mp3"), "Sheer Heart Attack", "Queen", "../imgs/covers/queen-newsoftheworld.jpeg")
-    ], "News of the World", "Queen", "../imgs/covers/queen-newsoftheworld.jpeg", "Rock")
+    ], "News of the World", "Queen", "../imgs/covers/queen-newsoftheworld.jpeg", "Rock"),
+
+    new Album([
+        new Song(new Audio("../songs/Crossroads.mp3"), "Crossroads", "Christopher Larkin", "../imgs/crossroads.jpg"),
+        new Song(new Audio("../songs/Dirtmouth.mp3"), "Dirtmouth", "Christopher Larkin", "../imgs/dirtmouth.jpg"),
+        new Song(new Audio("../songs/Enter Hallownest.mp3"), "Enter Hallownest", "Christopher Larkin", "../imgs/enterhallownest.jpg")
+    ], "Hollow Knight", "Christopher Larkin", "../imgs/covers/hollowknight.jpeg", "Game Soundtrack"),
+
+    new Album([
+        new Song(new Audio("../songs/Wonderwall.mp3"), "Wonderwall", "Oasis", "../imgs/wonderwall.jpg"),
+        new Song(new Audio("../songs/Don't Look Back In Anger .mp3"), "Don't Look Back in Anger", "Oasis", "../imgs/dontlookbackinanger.jpg"),
+        new Song(new Audio("../songs/Champagne Supernova.mp3"), "Champagne Supernova", "Oasis", "../imgs/champagnesupernova.jpg")
+    ], "What's the Story Morning Glory?", "Oasis", "../imgs/covers/oasis.jpeg", "Britpop")
 ]
+
 
 
 let likes = JSON.parse(localStorage.getItem("likes")) || [];
