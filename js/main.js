85d6bbd3c4946f29d2c6705d78aabb4a25db2c75
LianIvanSinchez <maquiavelic1030@gmail.com>
2024-07-02 16:12:35 -0300

    parche al addalbum

--- js/main.js
+++ js/main.js
@@ -282,10 +282,6 @@ function addAlbum(index) {
             modal.style.display = "block";
         }
     
-        span.onclick = () => {
-            modal.style.display = "none";
-        }
-    
         window.onclick = (event) => {
             if (event.target == modal) {
                 modal.style.display = "none";
